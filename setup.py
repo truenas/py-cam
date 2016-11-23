e4567ee63bd677d7c503dd6f2b143462812ed49a
Jakub Klama <jakub@ixsystems.com>
2016-11-23 03:15:28 +0100

    Add basic iSCSI initiator support.

--- setup.py
+++ setup.py
@@ -50,6 +50,11 @@ extensions = [
         "ctl",
         ["ctl.pyx"],
         include_dirs=system_includes
+    ),
+    Extension(
+        "iscsi",
+        ["iscsi.pyx"],
+        include_dirs=system_includes
     )
 ]
 
